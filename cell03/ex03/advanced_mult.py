5129dc63f786ac823584ab93ada5f4a77a7d35fa
Saharhaast <66070313@kmitl.ac.th>
2026-09-21 17:20:49 +0700

    Create advanced_mult.py

---
+++ cell03/ex03/advanced_mult.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+table = 0
+
+while table <= 10:
+    print(f"Table de {table}:", end="")
+    multiplier = 0
+
+    while multiplier <= 10:
+        print(f" {table * multiplier}", end="")
+        multiplier += 1
+
+    print()
+    table += 1
