8bd553b08ec1d188eeac76d3094e2ef0c0d9e286
Saharhaast <66070313@kmitl.ac.th>
2026-09-21 13:51:51 +0700

    yes

---
+++ cell01/ex00/name.py
@@ -0,0 +1,4 @@
+first_name = "Saharat"
+last_name = "Rattanachai"
+
+print(first_name, last_name)
---
+++ cell01/ex01/name.py
@@ -0,0 +1,6 @@
+first_name = "Saharat"
+last_name = "Rattanachai"
+
+whole_name = last_name + " " + first_name
+
+print(whole_name)
