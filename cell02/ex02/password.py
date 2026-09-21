b7012fb8879be5c8cfb51a1d5d9a394fee4dc20d
Saharhaast <66070313@kmitl.ac.th>
2026-09-21 15:34:31 +0700

    yh

--- cell02/ex02/password.py
+++ cell02/ex02/password.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 password = "Python is awesome"
 user_input = input()
 
--- cell02/ex03/mult.py
+++ cell02/ex03/mult.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 first_num = int(input("Enter the first number:\n"))
 second_num = int(input("Enter the second number:\n"))
 
